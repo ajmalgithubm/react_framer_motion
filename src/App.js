25ab1b4923572f611c77468868802b5ca10c7114
ajmalgithubm <ajmalm76774@gmail.com>
2023-08-18 15:45:06 +0530

    route changed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+
+
+function Home() {
+  return (
+    <div className="home component">
+      <h1>Home Component</h1>
+    </div>
+  )
+}
+
+
+function Header() {
+  return (
+    <div className="header">
+      <span>Header component</span>
+      <ul>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/about'>About</NavLink>
+        <NavLink to='/contact'>Contact</NavLink>
+
+      </ul>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div className="about component">
+      <h1>About Component</h1>
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div className="contact component">
+      <h1>Contact Component</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
